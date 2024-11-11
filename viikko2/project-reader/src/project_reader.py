111aec53c3e354205c545eace363241f3b3a380c
Virpi Lohtander <virpimaritta@gmail.com>
2024-11-11 18:33:42 +0200

    muutokset vko2

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,15 @@
+from urllib import request
+from project import Project
+import toml
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        print(content)
+        parsed = toml.load(content)
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(parsed['name'], "Test description", [], [])
